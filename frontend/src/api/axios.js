1b9ab2eea696b79eb56c42245a666a642a184aaa
Ali <74460721+ayazono@users.noreply.github.com>
2025-10-31 06:24:58 +0800

    added axios for vite employment

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,9 @@
+// frontend/src/api/axios.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE,
+  withCredentials: true,
+});
+
+export default api;
